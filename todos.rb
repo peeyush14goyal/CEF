b740ecf7c08f64c08823402b369a6c531f6ae248
Peeyush <peeyushgoyal154@gmail.com>
2021-01-16 13:02:11 +0530

    Ruby variables and comments

---
+++ todos.rb
@@ -0,0 +1,14 @@
+# ask for a list of todos
+# and print them back to us
+
+puts "What is your next todo?"
+
+#get string
+next_todo = gets
+
+puts "So this is your next todo.."
+puts next_todo
+
+puts "\n\n\n" #Carriage Return
+
+puts "[ ] #{next_todo}" # String Interpolation
